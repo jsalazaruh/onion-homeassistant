13fa6ecb333548a92cd7464aa2376c14349f2eb4
Jonathan Salazar <jonathansalazar94@gmail.com>
2018-04-02 18:56:04 -0500

    Update climate.py

--- climate.py
+++ climate.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
+
 import subprocess
 import json
 import time
 import paho.mqtt.publish as publish
 
-#loads temp & humidity with sensor and saves in JSON format
+#loads temp & humidity with sensor and saves in JSON format, make sure you change the pin number
 first = json.loads(subprocess.check_output('dht-sensor 3 DHT11 json',shell=True))
 time.sleep(6)
 second = json.loads(subprocess.check_output('dht-sensor 3 DHT11 json',shell=True))
@@ -17,7 +19,7 @@ print(temperature1, temperature2)
 temperature2 = second['temperature']
 
 #comparing data in order to verify authenticity of data
-if(abs(humidity1 - humidity2) > 2 or abs(temperature1 - temperature2) > 2 or temperature1 < -200 or temperature2 < -200or humidity1 < -200 or humidity2 < -200):
+if(abs(humidity1 - humidity2) > 2 or abs(temperature1 - temperature2) > 2 or temperature1 < -200 or temperature2 < -200 or humidity1 < -200 or humidity2 < -200):
  print("Problem")
 else:
  str = json.dumps(second)
