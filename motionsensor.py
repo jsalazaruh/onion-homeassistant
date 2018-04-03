a921f7382916f6a06a4316628f0d64dc02e07a5f
Jonathan Salazar <jonathansalazar94@gmail.com>
2018-04-02 19:06:43 -0500

    Update motionsensor.py

--- motionsensor.py
+++ motionsensor.py
@@ -1,20 +1,23 @@
+#!/usr/bin/env python
+
 import time
 import onionGpio
 import paho.mqtt.publish as publish
 
+#initializing gpio and setting direction
 gpio1 = onionGpio.OnionGpio(1)
-
 gpio1.setInputDirection()
 
+#start of loop to detect motion
 while True:
-    value = gpio1.getValue().rstrip()
+
+    #conerting to string for boolean comparison
+    value = str(gpio1.getValue())
 
     if (value == "1"):
         print "Motion Detected"
-        publish.single("sensor/motion1", "ON", hostname="10.0.0.174", port=1883)
+        publish.single("enter_topic_here", "ON", hostname="enter_ip", port=1883)
         time.sleep(5)
-        publish.single("sensor/motion1", "OFF", hostname="10.0.0.174", port=1883
+        publish.single("enter_topic_here", "OFF", hostname="enter_ip", port=1883)
 
     time.sleep(0.1)
-
-~
