2487e5dd5f53892d77f97d3a615b998e7e3815eb
Jonathan Salazar <jonathansalazar94@gmail.com>
2018-04-02 19:37:49 -0500

    Update powermonitoring.py

--- powermonitoring.py
+++ powermonitoring.py
@@ -1,72 +1,75 @@
-import onionSpi
+#!/usr/bin/env python
+
 import time
+import onionSpi
 import struct
 
+#printing settings for spi bus
 def printSettings(obj):
-	print "SPI Device Settings:"
-	print "  bus:    %d"%(obj.bus)
-	print "  device: %d"%(obj.device)
-	print "  speed:    %d Hz (%d kHz)"%(obj.speed, obj.speed/1000)
-	print "  delay:    %d us"%(obj.delay)
-	print "  bpw:      %d"%(obj.bitsPerWord)
-	print "  mode:     %d (0x%02x)"%(obj.mode, obj.modeBits)
-	print "     3wire:    %d"%(obj.threewire)
-	print "     lsb:      %d"%(obj.lsbfirst)
-	print "     loop:     %d"%(obj.loop)
-	print "     no-cs:    %d"%(obj.noCs)
-	print "     cs-high:  %d"%(obj.csHigh)
-	print ""
-	print "GPIO Settings:"
-	print "  sck:      %d"%(obj.sck)
-	print "  mosi:     %d"%(obj.mosi)
-	print "  miso:     %d"%(obj.miso)
-	print "  cs:       %d"%(obj.cs)
-	print ""
+    print "SPI Device Settings:"
+    print "  bus:      %d"%(obj.bus)
+    print "  device:   %d"%(obj.device)
+    print "  speed:    %d Hz (%d kHz)"%(obj.speed, obj.speed/1000)
+    print "  delay:    %d us"%(obj.delay)
+    print "  bpw:      %d"%(obj.bitsPerWord)
+    print "  mode:     %d (0x%02x)"%(obj.mode, obj.modeBits)
+    print "  3wire:    %d"%(obj.threewire)
+    print "  lsb:      %d"%(obj.lsbfirst)
+    print "  loop:     %d"%(obj.loop)
+    print "  no-cs:    %d"%(obj.noCs)
+    print "  cs-high:  %d"%(obj.csHigh)
+    print " "
+    print "GPIO Settings:"
+    print "  sck:    %d"%(obj.sck)
+    print "  mosi:   %d"%(obj.mosi)
+    print "  miso:   %d"%(obj.miso)
+    print "  cs:     %d"%(obj.cs)
+    print " "
 
+#assigning the pins for spi bus and assigning the clock speed for 3.3V
 def initADC(miso, mosi, sclk, cs):
-	spiDev  = onionSpi.OnionSpi(1, 32766)
-	spiDev.delay = 10
-	spiDev.mode = 0
-	spiDev.sck = sclk
-	spiDev.mosi = mosi
-	spiDev.miso = miso
-	spiDev.cs = cs
-	spiDev.speed = 1350000 # for 3.3V supply
+    spiDev  = onionSpi.OnionSpi(1, 32766)
+    spiDev.delay = 10
+    spiDev.mode = 0
+    spiDev.sck = sclk
+    spiDev.mosi = mosi
+    spiDev.miso = miso
+    spiDev.cs = cs
+    spiDev.speed = 1350000 
 
-	spiDev.setVerbosity(0)
-	# check the device
-	print 'Checking if device exists...'
-	ret = spiDev.checkDevice()
-	print '   Device does not exist: %d'%(ret)
+    spiDev.setVerbosity(0)
+    print 'Checking if device exists...'
+    out = spiDev.checkDevice()
+    print '   Device does not exist: %d'%(out)
 
-	# register the device
-	print 'Registering the device...'
-	ret = spiDev.registerDevice()
-	print '   registerDevice returned: %d'%(ret)
+    print 'Registering the device...'
+    out = spiDev.registerDevice()
+    print '   registerDevice returned: %d'%(out)
 
-	# initialize the device parameters
-	print 'Initializing the device parameters...'
-	ret = spiDev.setupDevice()
-	print '   setupDevice returned: %d'%(ret)
+    print 'Initializing the device parameters...'
+    out = spiDev.setupDevice()
+    print '   setupDevice returned: %d'%(out)
 
-	# check the device again
-	print '\nChecking if device exists...'
-	ret = spiDev.checkDevice()
-	print '   Device does not exist: %d'%(ret)
-	return spiDev
+    print '\nChecking if device exists...'
+    out = spiDev.checkDevice()
+    print '   Device does not exist: %d'%(out)
 
+    return spiDev
+
+#function to obtain data 
 def poll_sensor(spiDev, channel):
 
     assert 0 <= channel <= 1, 'ADC channel must be 0 or 1.'
 
-    # first bit + single-ended with channel
+    #leading bit + 
     if channel == 1:
+	#11100000
         cbyte = 0xE0
     else:
+	#11000000
         cbyte = 0xC0
 
     #first bit and single with channel, readByte is already MSBF
-
     spiResp = spiDev.readBytes(cbyte, 3)
 
     byte0 = spiResp[0]
@@ -79,10 +82,9 @@ def poll_sensor(spiDev, channel):
 
     return 0x3FF & ((byte0 & 0x01) << 9 | (byte1 & 0xFF) << 1 | (byte2 & 0x80) >> 7 )
 
-#main
+#start of main program 
 spi = initADC(9,8,7,6)
-#printSettings(spi)
-:
+
 channel = 0
 adcValues = []
 for i in range(100):
@@ -91,7 +93,7 @@ for i in range(100):
     #volt = round (((monkey * 3300) / 1024), 0)
     print ('10 bit resolution: ' + str(monkey))
     #print ('Voltage (mV): ' + str(volt))
-    #curr = round((monkey * 0.029), 0)
-    #print ('Current (mA): ' + str(curr))
-
+    
+    #Sampling rate at 60Hz for hairdryer, becasue of Nyquist Theorem, we want to get 10 times the amount of samples
+    #to obtain an accurate reading. Anything above samplerate * 2 will be fine  
     time.sleep(0.0014)
