f72bbbd29a68a972031fe68f49d96bf574cc4278
Jonathan Salazar <jonathansalazar94@gmail.com>
2018-05-16 14:59:07 -0500

    Update dhtSensor.py

--- Onions/dhtSensor.py
+++ Onions/dhtSensor.py
@@ -3,24 +3,27 @@ import time
 import paho.mqtt.publish as publish
 import json
 
+#Start loop for digital humidity temperature sensor  
 while True:
     humidity1 = 0
     temperature1 = 0
-    process = subprocess.Popen(['dht-sensor', '0', 'DHT11', 'json'],
-    stdout=subprocess.PIPE)
+    #Using opkg package to read the dht sensor with onion and then saving the output in JSON format to 'process'
+    process = subprocess.Popen(['dht-sensor', '0', 'DHT11', 'json'],stdout=subprocess.PIPE)
 
     stdout = json.loads(process.communicate()[0])
 
-    #creating tuples for organization
+    #Creating tuples for organization
     humidity1 = stdout["humidity"]
     temperature1 = stdout["temperature"]
     print(humidity1, temperature1)
-#comparing data in order to verify authenticity of data
+    #Comparing data in order to verify authenticity of data
     if(temperature1 < -200 and humidity1 < -200):
         print("Problem")
     else:
         str = json.dumps(stdout)
         try:
+            #Trying to send json package to topic, if it doesn't reach a connection 
+            #it will pass the loop without returning any errors
             publish.single("YOUR_TOPIC_HERE", str, hostname="YOUR_IP_HERE", port=1
         except:
             pass
